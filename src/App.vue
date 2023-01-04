a0115b22d6a62d0a38ab92d3b51f13bdd3faed69
wnduddl30 <leejy3022@naver.com>
2023-01-05 01:00:28 +0900

    router

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <ToolBar></ToolBar>  -->
+    <tool-bar></tool-bar> <!-- 이걸 추천 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolBar from './components/ToolBar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    ToolBar,
+  },
+  methods:{
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  padding: 0;
+  margin: 0;
 }
 </style>
