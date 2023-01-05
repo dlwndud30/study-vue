b5ba49ea8d530d316f6322d8f352c50d6b4de82d
wnduddl30 <leejy3022@naver.com>
2023-01-05 23:08:31 +0900

    axios를 이용한 API 호출

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" :key="user">{{user}}</div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+    data(){
+        return{
+            users:[],
+        }
+    },
+    created(){
+        var vm = this;
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+            .then(function(response){
+                console.log(response);
+                vm.users = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+        })
+    }
 }
 </script>
 
