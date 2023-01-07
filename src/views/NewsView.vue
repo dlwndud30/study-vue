0a3dc43a2c9249373094528cd7ffcc70bc487059
wnduddl30 <leejy3022@naver.com>
2023-01-07 23:46:54 +0900

    actions / mutations 적용

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="(user,idx) in users" :key="idx">{{user.title}}</div>
+    <div v-for="(user,idx) in this.$store.state.news" :key="idx">{{user.title}}</div>
   </div>
 </template>
 
@@ -9,18 +9,16 @@ import {fetchNewsList} from '@/api/index';
 export default {
     data(){
         return{
-            users:[],
         }
     },
     created(){
-        console.log('호출 전: ', this);
+        //console.log('호출 전: ', this);
 
         // var vm =this;
         // fetchNewsList().then(function(response){
         //     //비동기함수에서의 this -> this: undefined
         //     //화살표 함수 사용 -> 호출 전 this와 호출 후 this가 동일
-        //     console.log('호출 후: ', this);
-        
+        //     console.log('호출 후: ', this);   
         //     console.log(response);
         //     vm.users = response.data;
         // })
@@ -28,17 +26,18 @@ export default {
         //     console.log(error);
         // });
 
-        fetchNewsList().then((response)=>{
-            //비동기함수에서의 this -> this: undefined
-            //화살표 함수 사용 -> 호출 전 this와 호출 후 this가 동일
-            console.log('호출 후: ', this);
-        
-            console.log(response);
-            this.users = response.data;
-        })
-        .catch(function(error){
-            console.log(error);
-        });
+        // fetchNewsList().then((response)=>{
+        //     //비동기함수에서의 this -> this: undefined
+        //     //화살표 함수 사용 -> 호출 전 this와 호출 후 this가 동일
+        //     console.log('호출 후: ', this); 
+        //     console.log(response);
+        //     this.users = response.data;
+        // })
+        // .catch(function(error){
+        //     console.log(error);
+        // });
+
+        this.$store.dispatch('FETCH_NEWS');
     }
 }
 </script>
