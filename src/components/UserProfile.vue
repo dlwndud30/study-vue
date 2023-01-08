e6b00fd679bca6f26ddff012ade3d72dbcd1ae2d
wnduddl30 <leejy3022@naver.com>
2023-01-09 00:38:48 +0900

    사용자 프로필 공통 컴포넌트화 - 데이터 흐름처리1

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fa fa-user-circle-o"></i>
+    </div>
+    <div class="user-description">
+      <div>
+        {{userInfo.id}}
+      </div>
+      <div class="time">
+        {{userInfo.created}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed:{
+    userInfo(){
+        return this.$store.state.user;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-container{
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user-circle-o{
+  font-size: 2.5rem;
+}
+.user-description{
+  padding-left: 8px;
+}
+.time{
+  font-size: 0.7rem;
+}
+</style>
