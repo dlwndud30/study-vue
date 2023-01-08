fa4933658587cce65477ce3bb2498ddf987db9ec
wnduddl30 <leejy3022@naver.com>
2023-01-09 00:38:40 +0900

    사용자 프로필 공통 컴포넌트화 - 데이터 흐름처리1

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <user-profile></user-profile>
     <p>
       name: {{userInfo.id}}
     </p>
@@ -13,19 +14,18 @@
 </template>
 
 <script>
-// import axios from 'axios';
+import UserProfile from '@/components/UserProfile.vue';
 
 export default {
+  components: { UserProfile },
   computed:{
     userInfo() {
       return this.$store.state.user;
     },
   },
   created(){
-    console.log(this.$route.params.id);
     const userName = this.$route.params.id;
     this.$store.dispatch('FETCH_USER', userName);
-    // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
   },
 }
 </script>
