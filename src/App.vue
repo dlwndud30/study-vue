13e6a8a8aa5207cf54f2b00411fe4e2e2c4020a8
wnduddl30 <leejy3022@naver.com>
2023-01-08 22:47:24 +0900

    라우터 트랜지션

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <!-- <ToolBar></ToolBar>  -->
     <tool-bar></tool-bar> <!-- 이걸 추천 -->
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -23,5 +25,15 @@ export default {
 body{
   padding: 0;
   margin: 0;
+}
+/* router transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
