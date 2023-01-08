86a237a7d180e1319d7c04f8fccc02b61c69c404
wnduddl30 <leejy3022@naver.com>
2023-01-08 23:23:28 +0900

    스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,13 +1,25 @@
 <template>
     <div>
-      <p v-for="(item,idx) in fetchedAsk" :key="idx">
-        <router-link :to="`/item/${item.id}`">
-            {{item.title}}
-        </router-link>
-        <small>
-            {{item.time_ago}} by {{item.user}}
-        </small>
-      </p>
+        <ul class="news-list">
+            <li v-for="(item,idx) in fetchedAsk" :key="idx" class="post">
+                <div class="points">
+                  {{item.points}}
+                </div>
+                <div>
+                    <p class="news-title">
+                      <router-link :to="`/item/${item.id}`" > 
+                        {{item.title}}
+                      </router-link>
+                    </p>
+                    <small class="link-text">
+                      {{item.time_ago}} by 
+                      <router-link :to="`/user/${item.user}`" class="link-text"> 
+                        {{item.user}}
+                      </router-link>
+                    </small>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -31,6 +43,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list{
+  margin: 0;
+  padding: 0;
+}
+.post{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points{
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.news-title{
+  margin: 0;
+}
+.link-text{
+  color: #828282;
+}
 </style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,14 +1,26 @@
 <template>
-   <div>
-    <p v-for="(item,idx) in this.$store.state.jobs" :key="idx">
-      <a :href="item.url">
-        {{item.title}}
-      </a>
-      <small>
-        {{item.time_ago}}, {{item.domain}}
-      </small>
-    </p>
-   </div>
+  <div>
+    <ul class="news-list">
+      <li v-for="(item,idx) in this.$store.state.jobs" :key="idx" class="post">
+        <div class="points">
+          {{item.points || 0}}
+        </div>
+        <div>
+          <p class="news-title">
+            <a :href="item.url">
+              {{item.title}}
+            </a>
+          </p>
+          <small class="link-text">
+            {{item.time_ago}}, 
+            <a :href="item.url">
+              {{item.domain}}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -24,6 +36,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list{
+  margin: 0;
+  padding: 0;
+}
+.post{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points{
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #41b883;
+}
+.news-title{
+  margin: 0;
+}
+.link-text{
+  color: #828282;
+}
 </style>
