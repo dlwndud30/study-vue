74c57be0a9ed0f2dd27b6e92bbf99f56deadf96a
wnduddl30 <leejy3022@naver.com>
2023-01-10 23:15:59 +0900

    오류 수정

--- src/mixins/ListMixin.js
+++ src/mixins/ListMixin.js
@@ -4,15 +4,14 @@ export default {
     //재사용할 컴포넌트 옵션&로직
     created(){
         bus.$emit('start:spinner');
-        setTimeout(()=>{
+        
             this.$store.dispatch('FETCH_LIST', this.$route.name)
             .then(()=>{
-                console.log('fetched');
+                console.log('5');
                 bus.$emit('end:spinner');
             })
             .catch((error)=>{
                 console.log(error);
             });
-        }, 1000);
     },
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,36 @@
 import {fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchItem, fetchList} from '@/api/index';
 
 export default {
-    // FETCH_NEWS(context){
-    //     fetchNewsList()
-    //       .then((response)=>{
-    //         context.commit('SET_NEWS', response.data);
-    //         return response;
-    //       })
-    //       .catch((error)=>{
-    //         console.log(error);
-    //       });
-    // },
-    // FETCH_ASK(context){
-    //     fetchAskList()
-    //     .then((response)=>{
-    //         context.commit('SET_ASK', response.data);
-    //     })
-    //     .catch((error)=>{
-    //         console.log(error);
-    //     });
-    // },
-    // FETCH_JOBS(context){
-    //     fetchJobsList()
-    //     .then((response)=>{
-    //         context.commit('SET_JOBS', response.data);
-    //     })
-    //     .catch((error)=>{
-    //         console.log(error);
-    //     })
-    // },
+    FETCH_NEWS(context){
+        return fetchNewsList()
+          .then((response)=>{
+            context.commit('SET_NEWS', response.data);
+            return response;
+          })
+          .catch((error)=>{
+            console.log(error);
+          });
+    },
+    FETCH_ASK(context){
+        return fetchAskList()
+        .then((response)=>{
+            context.commit('SET_ASK', response.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    },
+    FETCH_JOBS(context){
+        return fetchJobsList()
+        .then((response)=>{
+            context.commit('SET_JOBS', response.data);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    },
     FETCH_USER({commit}, payload) {
-        fetchUserInfo(payload)
+        return fetchUserInfo(payload)
         .then(({data})=>{
             commit('SET_USER', data)
         })
@@ -39,7 +39,7 @@ export default {
         })
     },
     FETCH_ITEM(context, payload){
-        fetchItem(payload)
+        return fetchItem(payload)
         .then((response)=>{
             context.commit('SET_ITEM', response.data)
         })
@@ -48,8 +48,9 @@ export default {
         })
     },
     FETCH_LIST({commit}, pageName){
-        fetchList(pageName)
+        return fetchList(pageName)
         .then(({data})=>{
+            console.log('4');
             commit('SET_LIST', data);
         })
         .catch((error)=>{
